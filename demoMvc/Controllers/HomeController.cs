8d0b98eabacfde484fb3294bf999e770ee92e8f2
NGUYEN VAN CUONG <1621050122@student.humg.edu.vn>
2021-10-28 09:14:57 +0700

    first commit

---
+++ demoMvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using demoMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace demoMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
